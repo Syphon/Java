c2720d25fc3943f5f532db78a063b45969dcef4e
codeanticode <andres.colubri@gmail.com>
2013-09-02 15:15:37 -0400

    added binaries compatile with OSX 10.6, updated readme

--- JSyphon/src/jsyphon/test/ServerTest.java
+++ JSyphon/src/jsyphon/test/ServerTest.java
@@ -12,10 +12,6 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
 /**
- * 
- */
-
-/**
  * @author Skye Book
  *
  */
@@ -31,91 +27,60 @@ public class ServerTest {
 		server=new JSyphonServer();
 	}
 
-
 	public void start() {
 		try {
 			Display.setDisplayMode(new DisplayMode(800,600));
 			Display.create();
+			Display.setTitle("ServerTest");
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
 
-		// init OpenGL here
-
 		server.initWithName("Server Test");
-		//IntBuffer dims = ByteBuffer.allocateDirect(16).order(ByteOrder.nativeOrder()).asIntBuffer();
+		
+    IntBuffer intBuff = ByteBuffer.allocateDirect(4).order(ByteOrder.nativeOrder()).asIntBuffer();
 
-		//IntBuffer dims = IntBuffer.allocate(16);
+    ByteBuffer dummy = null;
 
-		IntBuffer intBuff = ByteBuffer.allocateDirect(4).order(ByteOrder.nativeOrder()).asIntBuffer();
+    while (!Display.isCloseRequested()) {
+      // This copies the back buffer of LWJGL
 
-		ByteBuffer dummy = null;
+      // render OpenGL here
+      GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+      
+      GL11.glPointSize(40f);
+      GL11.glBegin(GL11.GL_POINTS);
+      GL11.glVertex3f(0.0f, 0.0f, 0.0f);
+      GL11.glEnd();
 
-		while (!Display.isCloseRequested()) {
+      // Its highly recommended you use RECTANGLE textures
+      int target = GL11.GL_TEXTURE_2D; // GL31.GL_TEXTURE_RECTANGLE or GL11.GL_TEXTURE_2D
+      
+      GL11.glEnable(target);
 
-			// This captures all of the drawing done by LWJGL
-			/*server.bindToDrawFrameOfSize(Display.getDisplayMode().getWidth(), Display.getDisplayMode().getHeight());
+      GL11.glGenTextures(intBuff);
 
-			// render OpenGL here
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+      GL11.glBindTexture(target, intBuff.get(0));
 
-			GL11.glPointSize(40f);
-			GL11.glBegin(GL11.GL_POINTS);
-			GL11.glVertex3f(0.0f, 0.0f, 0.0f);
-			GL11.glEnd();
+      GL11.glTexImage2D(target, 0, GL11.GL_RGBA8, 800, 600, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, dummy);
 
-			server.unbindAndPublish();
-			 */
+      GL11.glCopyTexSubImage2D(target, 0, 0, 0, 0, 0, 800, 600);
 
+      server.publishFrameTexture(intBuff.get(0), target, 0, 0, 800, 600, 800, 600, false);
+      
+      GL11.glDeleteTextures(intBuff.get(0));
 
-			// This copies the back buffer of LWJGL
+      intBuff.clear();
+      intBuff.rewind();
 
-			// render OpenGL here
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-			
-			GL11.glPointSize(40f);
-			GL11.glBegin(GL11.GL_POINTS);
-			GL11.glVertex3f(0.0f, 0.0f, 0.0f);
-			GL11.glEnd();
+      // Always call this so LWJGL can swap buffers, etc.
+      Display.update();
+    }
 
-			//dims.clear();
-			//dims.rewind();
-			//GL11.glGetInteger(GL11.GL_VIEWPORT, dims);
-
-			// Its highly recommended you use RECTANGLE textures
-			int target = GL11.GL_TEXTURE_2D; // GL31.GL_TEXTURE_RECTANGLE or GL11.GL_TEXTURE_2D
-			
-			GL11.glEnable(target);
-
-			GL11.glGenTextures(intBuff);
-
-			//GL11.glBindTexture(GL31.GL_TEXTURE_RECTANGLE, intBuff.get(0));
-			GL11.glBindTexture(target, intBuff.get(0));
-
-			//GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, dims.get(2), dims.get(3), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, dummy);
-			GL11.glTexImage2D(target, 0, GL11.GL_RGBA8, 800, 600, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, dummy);
-
-			//GL11.glCopyTexSubImage2D(GL11.GL_TEXTURE_2D, 0, 0, 0, dims.get(0), dims.get(1), dims.get(2), dims.get(3));
-			GL11.glCopyTexSubImage2D(target, 0, 0, 0, 0, 0, 800, 600);
-
-			//server.publishFrameTexture(intBuff.get(0), GL11.GL_TEXTURE_2D, 0, 0, dims.get(2), dims.get(3), dims.get(2), dims.get(3), false);
-			server.publishFrameTexture(intBuff.get(0), target, 0, 0, 800, 600, 800, 600, false);
-			
-			GL11.glDeleteTextures(intBuff.get(0));
-
-			intBuff.clear();
-			intBuff.rewind();
-
-
-			// Always call this so LWJGL can swap buffers, etc.
-			Display.update();
-		}
-
-		server.stop();
-		Display.destroy();
+    server.stop();
+    Display.destroy();
 	}
-
 
 	/**
 	 * @param args
@@ -124,6 +89,5 @@ public class ServerTest {
 	public static void main(String[] args) throws LWJGLException {
 		ServerTest t = new ServerTest();
 		t.start();
-
 	}
 }
